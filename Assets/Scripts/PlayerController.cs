0ebaf04323ade712765cf49abf2a80796bb0b82c
Annie Rudnick <a@rud.co>
2023-04-12 12:30:20 -0600

    remove silly logs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,28 +8,21 @@ public class PlayerController : MonoBehaviour
 
     public void OnYellow(InputValue value)
     {
-        //if (Input.GetButtonDown("Yellow"))
-        //{
-        Debug.Log("yellow pressed");
         GameManager.instance.addToSequence(1);
-        //}
     }
 
     public void OnRed(InputValue value)
     {
-        Debug.Log("red pressed");
         GameManager.instance.addToSequence(0);
     }
 
     public void OnGreen(InputValue value)
     {
-        Debug.Log("green pressed");
         GameManager.instance.addToSequence(2);
     }
 
     public void OnBlue(InputValue value)
     {
-        Debug.Log("blue pressed");
         GameManager.instance.addToSequence(3);
     }
 }
